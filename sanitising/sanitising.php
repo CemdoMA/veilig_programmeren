1e608fbb0c03361cfc854715c54ce7acf41127e2
CemdoMA <22315@ma-web.nl>
2017-10-03 11:30:29 +0200

    Update sanitising.php

--- sanitising/sanitising.php
+++ sanitising/sanitising.php
@@ -7,4 +7,12 @@ if(isset($_REQUEST["email"])
 	$email=substr($email,0,50);// no longer than 50.
 	echo "EMAIL: ".$email."<br>"; 
 }
+/*cem*/
+   if(isset($_REQUEST["email"])
+{
+	$email = filter_var($_REQUEST["email"], FILTER_SANITIZE_EMAIL);
+	$email=substr($email,0,50);// no longer than 50.
+	echo "EMAIL: ".$email."<br>"; 
+}
+/*cem*/
 ?>
